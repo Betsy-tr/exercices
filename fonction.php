b9c2e4ce961690a266d61eacdd86c5fba3c4a5f1
Betsy-tr <betsytramma@gmail.com>
2023-03-28 10:57:58 -0400

    first commit

---
+++ fonction.php
@@ -0,0 +1,31 @@
+<?php
+
+// Exercice I
+
+// Exercice II
+
+// Exercice III
+
+// Exercice IV
+
+// Exercice V
+
+// Exercice VI
+
+// Exercice VII
+
+// Exercice VIII
+
+// Exercice IX
+
+// Exercice X
+
+// Exercice XI
+
+// Exercice XII
+
+// Exercice XIII
+
+// Exercice XIV
+
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// On fait appel à  la founction              de  I
+
+// On fait appel à  la founction              de II
+
+// On fait appel à  la founction              de III
+
+// On fait appel à  la founction              de IV
+
+// On fait appel à  la founction              de V
+
+// On fait appel à  la founction              de VI
+
+// On fait appel à  la founction              de VII
+
+// On fait appel à  la founction              de VIII
+
+// On fait appel à  la founction              de IX
+
+// On fait appel à  la founction              de X
+
+// On fait appel à  la founction              de XI
+
+// On fait appel à  la founction              de XII
+
+// On fait appel à  la founction              de XIII
+
+// On fait appel à  la founction              de XIV
+
+?>
