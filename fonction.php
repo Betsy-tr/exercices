8e05b964a9df666cadd029d98c8bfaa5b720bc38
Betsy-tr <betsytramma@gmail.com>
2023-03-28 14:04:42 -0400

    exo terminé

--- fonction.php
+++ fonction.php
@@ -1,31 +1,188 @@
 <?php
 
 // Exercice I
+function helloWorld(){
+    return " Hello World ! " . '<br><br>' ; 
+}
 
 // Exercice II
+function quiEstLeMeilleurProf(){
+    return " Le prof de programmation web " . '<br><br>' ; 
+}
 
 // Exercice III
+function jeRetourneMonArgument( $Arg = 'abc' ){
+    return $Arg . '<br><br>' ;
+}
 
 // Exercice IV
+function concatenation( $nom = "Griezman" , $prenom = "Antoine" ){
+    return $prenom . $nom . '<br><br>' ;
+}
 
 // Exercice V
+function concatenationAvecEspace( $nom = "Ngolo" , $prenom = "Kante" ){
+    return $nom . " " . $prenom . '<br><br>' ;
+}
 
 // Exercice VI
+function somme( $a = 5 , $b = 5 ){
+    return $a + $b . '<br><br>' ;
+}
 
 // Exercice VII
+function soustration( $a = 5 , $b = 5 ){
+    return $a - $b . '<br><br>' ;
+}
 
 // Exercice VIII
+function multiplication( $a = 5 , $b = 5 ){
+    return $a * $b . '<br><br>' ;
+}
 
 // Exercice IX
+function estIlMajeur( $a = 34 ){
+    if( $a >= 18 ){
+        return true . '<br><br>' ;
+    } else {
+        return false . '<br><br>' ;
+    }
+}
+
 
 // Exercice X
+function plusGrand( $a = 10 , $b = 8 ){
+    if ($a > $b){
+        return $a . '<br><br>' ; 
+    }else {
+        return $b . '<br><br>' ;
+    }
+}
 
 // Exercice XI
+function plusPetit( $a = 10 , $b = 8 ){
+    if ($a < $b){
+        return $a . '<br><br>' ; 
+    }else {
+        return $b . '<br><br>' ;
+    }
+}
 
 // Exercice XII
+function vraimentPlusPetit( $a = 10 , $b = 8 , $c = 12 ){
+    if ($a < $b && $a < $c ){
+        return $a . '<br><br>' ; 
+    }elseif ($b < $a && $b < $c) {
+        return $b . '<br><br>' ;
+    }else {
+        return $c . '<br><br>' ;
+    }
+}
 
 // Exercice XIII
+function premierElementTableau( $monTableau ){
+    if( count($monTableau) > 0 ){
+        return $monTableau[0] . '<br><br>' ; 
+    }else {
+        return "null" . '<br><br>' ; 
+    }
+}
 
 // Exercice XIV
+function dernierElementTableau( $monTableau ){
+    if( count($monTableau) - 1 > 0 ){
+        return count($monTableau)-1 . '<br><br>' ; 
+    }else {
+        return "null" . '<br><br>' ; 
+    }
+}
+
+// Exercice XV
+function vraimentPlusGrand( $monTableau ){
+    if( count($monTableau) < 0 ){
+        return null . '<br><br>' ;
+    }elseif( $monTableau[0] > $monTableau[1] && $monTableau[0] > $monTableau[2]){
+        return $monTableau[0] . '<br><br>' ;
+    }elseif( $monTableau[1] > $monTableau[0] && $monTableau[1] > $monTableau[2] ){
+        return $monTableau[1] . '<br><br>' ;
+    }else{
+        return $monTableau[2] . '<br><br>' ;
+    }
+}
+// Exercice XVI
+function carrémentPlusPetit( $monTableau ){
+    if( count($monTableau) < 0 ){
+        return null . '<br><br>' ;
+    }elseif( $monTableau[0] < $monTableau[1] && $monTableau[0] < $monTableau[2]){
+        return $monTableau[0] . '<br><br>' ;
+    }elseif( $monTableau[1] < $monTableau[0] && $monTableau[1] < $monTableau[2] ){
+        return $monTableau[1] . '<br><br>' ;
+    }else{
+        return $monTableau[2] . '<br><br>' ;
+    }
+}
+// Exercice XVII
+function verificationPassword( $password = "digitalisation" ){
+    if( strlen($password) >= 8){
+        return true . '<br><br>' ;
+    }else{
+        return false . '<br><br>' ;
+    }
+}
+
+// Exercice XVIII
+function doubleVerificationPassword( $password = "digitalisation" ){
+    
+    $majuscule = preg_match('@ [A-Z] @', $password) ;
+    $minuscule = preg_match('@ [a-z] @', $password) ;
+    $chiffre = preg_match('@ [0-9] @', $password) ;
+
+    if( $majuscule || $minuscule || $chiffre || strlen($password) >= 8 ){
+        return true . '<br><br>' ;
+    }else{
+        return false . '<br><br>' ;
+    }
+}
+// Exercice XIX
+function capital( $pays ){
+    switch ( $pays ) {
+        case 'France':
+            return "Paris" . '<br><br>' ;
+            break ;
+        case 'Allemagne':
+            return "Berlin" . '<br><br>' ;
+            break ;
+        case 'Italie':
+            return "Rome" . '<br><br>' ;
+            break ;
+        case 'Maroc':
+            return "Rabat" . '<br><br>' ;
+            break ; 
+        case 'Espagne':
+            return "Madrid" . '<br><br>' ;
+            break ;  
+        case 'Portugal':
+            return "Lisbonne" . '<br><br>' ;
+            break ;
+        case 'Angleterre':
+            return "Londres" . '<br><br>' ;
+            break ; 
+        default:
+            return "Inconnu" . '<br><br>' ;
+            break ;
+    }
+}
+// Exercice XX
+function listHTML( $nomListe , $monTableau ){
+    $i = 0 ;
+
+    echo "<h3>" . $nomListe . "</h3>" . "<ul>" ;
+
+    while($i < count($monTableau)){
+        echo "<li>" . $monTableau[$i] . "</li>" . "</ul>" ;
+        $i++ ; 
+    }
+}
+
 
 ?>
--- index.php
+++ index.php
@@ -1,31 +1,71 @@
 <?php
 
-// On fait appel à  la founction              de  I
+include 'fonction.php' ; 
 
-// On fait appel à  la founction              de II
+// On fait appel à la founction helloWorld de I
+echo helloWorld() ; 
 
-// On fait appel à  la founction              de III
+// On fait appel à la founction quiEstLeMeilleurProf de II
+echo quiEstLeMeilleurProf() ; 
 
-// On fait appel à  la founction              de IV
+// On fait appel à la founction jeRetourneMonArgument de III
+echo jeRetourneMonArgument() ; 
 
-// On fait appel à  la founction              de V
+// On fait appel à  la founction concatenation de IV
+echo concatenation() ; 
 
-// On fait appel à  la founction              de VI
+// On fait appel à la founction concatenationAvecEspace de V
+echo concatenationAvecEspace() ;
 
-// On fait appel à  la founction              de VII
+// On fait appel à la founction somme de VI
+echo somme() ; 
 
-// On fait appel à  la founction              de VIII
+// On fait appel à la founction soustration de VII
+echo soustration() ;
 
-// On fait appel à  la founction              de IX
+// On fait appel à la founction multiplication de VIII
+echo multiplication() ;
 
-// On fait appel à  la founction              de X
+// On fait appel à la founction estIlMajeur de IX
+echo estIlMajeur() ; 
 
-// On fait appel à  la founction              de XI
+// On fait appel à la founction plusGrand de X
+echo plusGrand() ; 
 
-// On fait appel à  la founction              de XII
+// On fait appel à la founction plusPetit de XI
+echo plusPetit() ;
 
-// On fait appel à  la founction              de XIII
+// On fait appel à la founction vraimentPlusPetit de XII
+echo vraimentPlusPetit() ; 
 
-// On fait appel à  la founction              de XIV
+// On fait appel à la founction premierElementTableau de XIII
+$monTableau = [] ;
+echo premierElementTableau( $monTableau ) ;
+
+// On fait appel à la founction dernierElementTableau de XIV
+$monTableau = [] ;
+echo dernierElementTableau( $monTableau ) ;
+
+// On fait appel à la founction vraimentPlusGrand de XV
+$monTableau = [ 5 , 14 , 8 ] ;
+echo vraimentPlusGrand( $monTableau ) ;
+
+// On fait appel à la founction carrémentPlusPetit de XVI
+$monTableau = [ 5 , 14 , 8 ] ;
+echo carrémentPlusPetit( $monTableau ) ;
+
+// On fait appel à la founction verificationPassword de XVII
+echo verificationPassword() ;
+// On fait appel à la founction doubleVerificationPassword de XVIII
+echo doubleVerificationPassword() ;
+
+// On fait appel à la founction capital de XIX
+$pays = "Allemagne" ;
+echo capital( $pays ) ;
+
+// On fait appel à la founction listHTML de XX
+$nomListe = "Capitale" ;
+$monTableau = ["Madrid" , "Lisbonne" , "Londre"] ;
+echo listHTML( $nomListe , $monTableau ) ;
 
 ?>
